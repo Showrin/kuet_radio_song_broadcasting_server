57afaf7a9214033b0f4eefd3590c4916beee69c6
Showrin <showrinece15@gmail.com>
2020-05-08 14:41:09 +0600

    Change port config

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 const path = require("path");
-const port = 5000;
+const port = process.env.PORT || "5000";
 
 app.use(express.static("public"));
 
