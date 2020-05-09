18bc909ea8d774cde87bcdf844aef66d70737db8
Showrin <showrinece15@gmail.com>
2020-05-09 21:15:50 +0600

    Looping test

--- index.js
+++ index.js
@@ -1,7 +1,20 @@
+const https = require("https");
+const axios = require("axios");
 const express = require("express");
 const app = express();
 const path = require("path");
 const port = process.env.PORT || "5000";
+const getSonglistOptions = {
+  hostname: "www.kuetradio.org",
+  path: "/api/get_songlist.php",
+  method: "GET",
+};
+
+let songlist;
+let playingSong = {
+  id: 0,
+  played: 0,
+};
 
 app.use(express.static("public"));
 
@@ -10,3 +23,50 @@ app.get("/", (req, res) =>
 );
 
 app.listen(port, () => console.log("Server has been started"));
+
+async function getSongList() {
+  songlist = await axios
+    .get("http://www.kuetradio.org/api/get_songlist.php")
+    .then((response) => {
+      return response.data;
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+
+  console.log(songlist.length);
+
+  // for (let i = 0; ; i++) {
+  //   setTimeout(() => {
+  //     playingSong.id = songlist[i].id;
+  //     playingSong.played = 0;
+
+  //     console.log(playingSong);
+
+  //     if (i === songlist.length - 1) {
+  //       i = -1;
+  //       console.log("Reset i: ", i);
+  //     }
+  //   }, 1000);
+  // }
+
+  function songLoop(i = 0) {
+    console.log(playingSong);
+    setTimeout(() => {
+      playingSong.id = songlist[i].id;
+      playingSong.played = 0;
+
+      if (i === songlist.length - 1) {
+        i = 0;
+      } else {
+        i++;
+      }
+
+      songLoop(i);
+    }, 1000);
+  }
+
+  songLoop();
+}
+
+getSongList();
