958848ba02067867bb807106b9df6e6d6d4a2a6c
Showrin <showrinece15@gmail.com>
2020-05-09 17:01:40 +0600

    Remove database connectio

--- index.js
+++ index.js
@@ -1,15 +1,7 @@
 const express = require("express");
 const app = express();
 const path = require("path");
-const mysql = require("mysql");
 const port = process.env.PORT || "5000";
-const dbConnection = mysql.createConnection({
-  host: "localhost",
-  user: "kuetradi_admin",
-  password: "]r1TvU(tXHd6",
-  database: "kuetradi_kuet_radio_database",
-  port: 3306,
-});
 
 app.use(express.static("public"));
 
@@ -18,16 +10,3 @@ app.get("/", (req, res) =>
 );
 
 app.listen(port, () => console.log("Server has been started"));
-
-dbConnection.connect(function (err) {
-  console.log("requesting!");
-  if (err) throw err;
-  else {
-    console.log("Connected!");
-    let sql = "SELECT * FROM users WHERE id = 20";
-    dbConnection.query(sql, function (err, result) {
-      if (err) throw err;
-      console.log(result);
-    });
-  }
-});
